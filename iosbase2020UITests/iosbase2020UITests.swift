00c2f2148882f561ef89796c4c8c4ac28d4592c4
Robert Polovitzer <robert@agyl.ca>
2020-08-30 17:06:40 -0400

    Small UI Test

--- iosbase2020UITests/iosbase2020UITests.swift
+++ iosbase2020UITests/iosbase2020UITests.swift
@@ -9,35 +9,30 @@
 import XCTest
 
 class iosbase2020UITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    
+    var app = XCUIApplication()
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.continueAfterFailure = false
+        self.app = XCUIApplication()
+        self.app.launch()
     }
-
-    override func tearDownWithError() throws {
+    
+    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSelection() {
+        sleep(1)
+        self.selectFirstCell(identifier: "FactoryCell")
+        self.app.swipeUp()
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func selectFirstCell(identifier: String) {
+        let cell = self.app.cells.element(boundBy: 0)
+        cell.tap()
     }
 }
